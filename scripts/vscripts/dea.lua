52db5476a518c0e188e48cf1c25a73140eb18b6c
DEAFPS <43534349+deaFPS@users.noreply.github.com>
2023-09-23 10:07:36 +0200

    Add files via upload

--- scripts/vscripts/dea.lua
+++ scripts/vscripts/dea.lua
@@ -6,6 +6,7 @@ require "whitelist"
 require "pug_cfg"
 
 roundStarted = false
+currentMvmntSettings = "VNL"
 
 local adminPlayers = {
 	--admins/constantly whitelisted
@@ -61,9 +62,75 @@ function tableContains(table, value)
     return false
 end
 
+function mvmntSettings(setting)
+	if setting == "kz" then
+		SendToServerConsole("sv_accelerate 6.5")
+		SendToServerConsole("sv_accelerate_use_weapon_speed 0")
+		SendToServerConsole("sv_airaccelerate 100.0")
+		SendToServerConsole("sv_air_max_wishspeed 30.0")
+		SendToServerConsole("sv_enablebunnyhopping 1")
+		SendToServerConsole("sv_friction 5.0")
+		SendToServerConsole("sv_gravity 800.0")
+		SendToServerConsole("sv_jump_impulse 301.993377")
+		SendToServerConsole("sv_ladder_scale_speed 1.0")
+		SendToServerConsole("sv_ledge_mantle_helper 0.0")
+		SendToServerConsole("sv_maxspeed 320.0")
+		SendToServerConsole("sv_maxvelocity 2000.0")
+		SendToServerConsole("sv_staminajumpcost 0.0")
+		SendToServerConsole("sv_staminalandcost 0.0")
+		SendToServerConsole("sv_staminamax 0.0")
+		SendToServerConsole("sv_staminarecoveryrate 0.0")
+		SendToServerConsole("sv_standable_normal 0.7")
+		SendToServerConsole("sv_timebetweenducks 0.0")
+		SendToServerConsole("sv_walkable_normal 0.7")
+		SendToServerConsole("sv_wateraccelerate 10.0")
+		SendToServerConsole("sv_water_movespeed_multiplier 0.8")
+		SendToServerConsole("sv_water_swim_mode  0.0")
+		SendToServerConsole("sv_weapon_encumbrance_per_item 0.0")
+		SendToServerConsole("sv_weapon_encumbrance_scale 0.0")
+		currentMvmntSettings = "KZ"
+	end
+	
+	if not setting == "vnl" then
+		SendToServerConsole("sv_accelerate 5.5")
+		SendToServerConsole("sv_accelerate_use_weapon_speed 1")
+		SendToServerConsole("sv_airaccelerate 12.0")
+		SendToServerConsole("sv_air_max_wishspeed 30.0")
+		SendToServerConsole("sv_enablebunnyhopping 1")
+		SendToServerConsole("sv_friction 5.2")
+		SendToServerConsole("sv_gravity 800.0")
+		SendToServerConsole("sv_jump_impulse 301.993377")
+		SendToServerConsole("sv_ladder_scale_speed 0.78")
+		SendToServerConsole("sv_ledge_mantle_helper 0.0")
+		SendToServerConsole("sv_maxspeed 320.0")
+		SendToServerConsole("sv_maxvelocity 3500.0")
+		SendToServerConsole("sv_staminajumpcost 0.08")
+		SendToServerConsole("sv_staminalandcost 0.05")
+		SendToServerConsole("sv_staminamax 80.0")
+		SendToServerConsole("sv_staminarecoveryrate 60.0")
+		SendToServerConsole("sv_standable_normal 0.7")
+		SendToServerConsole("sv_timebetweenducks 0.4")
+		SendToServerConsole("sv_walkable_normal 0.7")
+		SendToServerConsole("sv_wateraccelerate 10.0")
+		SendToServerConsole("sv_water_movespeed_multiplier 0.8")
+		SendToServerConsole("sv_water_swim_mode  0.0")
+		SendToServerConsole("sv_weapon_encumbrance_per_item 0.85")
+		SendToServerConsole("sv_weapon_encumbrance_scale 0.0")
+		currentMvmntSettings = "VNL"
+	end
+	
+end
+
 function StartWarmup()
 	SendToServerConsole("bot_kick")
 	SendToServerConsole("mp_warmuptime " .. warmupTime)
+	
+	if kzsettingsinwarmup then
+		mvmntSettings("kz")
+	else 
+		mvmntSettings("vnl")
+	end
+	
 end
 
 function RestartWarmup()
@@ -76,6 +143,13 @@ function RestartWarmup()
 	HC_PrintChatAll("{green} Restarting Warmup...")
 	HC_PrintChatAll("{green} Restarting Warmup...")
 	HC_PrintChatAll("{green} Restarting Warmup...")
+	
+	if kzsettingsinwarmup == "kz" then
+		mvmntSettings("kz")
+	else 
+		mvmntSettings("vnl")
+	end
+	
 	roundStarted = false
 end
 
@@ -89,6 +163,13 @@ function StartPug()
 	HC_PrintChatAll("{green} Starting Pug...")
 	HC_PrintChatAll("{green} Starting Pug...")
 	HC_PrintChatAll("{green} Starting Pug...")
+	
+	if kzsettings == "kz" then
+		mvmntSettings("kz")
+	else 
+		mvmntSettings("vnl")
+	end
+	
 	roundStarted = true
 end
 
