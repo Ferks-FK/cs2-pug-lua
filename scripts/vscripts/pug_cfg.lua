ea9ca796fca829a378d6cf493b42d92f2151fff4
DEAFPS <43534349+deaFPS@users.noreply.github.com>
2023-09-25 17:48:03 +0200

    Update pug_cfg.lua

--- scripts/vscripts/pug_cfg.lua
+++ scripts/vscripts/pug_cfg.lua
@@ -1,6 +1,8 @@
 chatPrefix = "{red} [DEAFPS PUG] " --appears before every plugin related chat msg
 warmupTime = "234124235" --default is infinite
 
+adminPassword = "123"
+
 enableWhitelist = true
 kzsettingsinwarmup = true
 kzsettings = false
