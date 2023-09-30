b428ea1568eba22a0ae5924b9668ffabac8cd93e
DEAFPS <43534349+deaFPS@users.noreply.github.com>
2023-09-30 21:35:38 +0200

    Update pug_cfg.lua

--- scripts/vscripts/pug_cfg.lua
+++ scripts/vscripts/pug_cfg.lua
@@ -2,10 +2,13 @@ chatPrefix = "{red} [DEAFPS PUG] " --appears before every plugin related chat ms
 warmupEndless = true
 warmupTime = "234124235" --default is infinite
 
-adminPassword = "zyzz"
+adminPassword = "123" --change this to your desired admin pw
 
-enableWhitelist = true
-kzsettingsinwarmup = true
-kzsettings = false
-teamSize = 5
-votingEnabled = true
+enableWhitelist = true --if whitelist shall be on or off
+kzsettingsinwarmup = true --whacky kz settings in warmup only
+kzsettings = false --whacky kz settings globally
+teamSize = 5 --amount of players per team
+votingEnabled = true --if players are able to vote to start the pug
+
+timeoutsPerTeam = 3 --amount of timeout votes allowed per team
+timeoutDuration = 30 --timeout duration
