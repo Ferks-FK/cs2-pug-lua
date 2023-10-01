3a54ae5ec0a7deb3837ee4506b53ad7f2aafd92e
DEAFPS <43534349+deaFPS@users.noreply.github.com>
2023-10-01 19:29:45 +0200

    Update banlist.lua

--- scripts/vscripts/banlist.lua
+++ scripts/vscripts/banlist.lua
@@ -1,5 +1,5 @@
 bannedPlayers = {
 	--players ip adresses to be autokicked when whitelist is disabled
-	"1.123.123.123:00000", --example
-	"1.123.123.123:00000", --example
+	"[U:1:00000000]", --example
+	"[U:1:00000000]", --example
 }
