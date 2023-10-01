56ee9f856ead0f197f630fbf49c3971416e4c801
DEAFPS <43534349+deaFPS@users.noreply.github.com>
2023-10-01 18:59:50 +0200

    Update pug_cfg.lua

--- scripts/vscripts/pug_cfg.lua
+++ scripts/vscripts/pug_cfg.lua
@@ -1,14 +1,20 @@
-chatPrefix = "{red} [DEAFPS PUG] " --appears before every plugin related chat msg
+chatPrefix = "{red} [DEAFPS PUG] "      --appears before every plugin related chat msg
 warmupEndless = true
-warmupTime = "234124235" --default is infinite
+warmupTime = "234124235"                --default is infinite
 
-adminPassword = "123" --change this to your desired admin pw
+adminPassword = "123"                   --change this to your desired admin pw
 
-enableWhitelist = true --if whitelist shall be on or off
-kzsettingsinwarmup = true --whacky kz settings in warmup only
-kzsettings = false --whacky kz settings globally
-teamSize = 5 --amount of players per team
-votingEnabled = true --if players are able to vote to start the pug
+enableWhitelist = true                  --if whitelist shall be on or off
+kzsettingsinwarmup = true               --whacky kz settings in warmup only
+kzsettings = false                      --whacky kz settings globally
+teamSize = 5                            --amount of players per team
+votingEnabled = true                    --if players are able to vote to start the pug
 
-timeoutsPerTeam = 3 --amount of timeout votes allowed per team
-timeoutDuration = 30 --timeout duration
+timeoutsPerTeam = 3                     --amount of timeout votes allowed per team
+timeoutDuration = 30                    --timeout duration
+
+
+useCustomCFG = false 					          -- if a custom cfg should be used
+
+customCFG = "crazywhackysettings.cfg" 	-- place your custom cfg into the cfg folder and put name here.
+										                    -- make sure to not use mp_restartgame since that is being handled by the plugin already
